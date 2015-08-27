7053fa54782c40ea04d39adfcbff5f32d1ec2908
Justin Castilla <pxlperfection@gmail.com>
2015-08-27 12:14:23 -0700

    added a good mongoDBler.js

--- app/models/mongoDBler.js
+++ app/models/mongoDBler.js
@@ -1,2 +1,37 @@
-var mongoose = require("mongoose");
+var mongoose = require("mongoose"); 
 mongoose.connect("mongodb://localhost/test");
+
+var ourBooks = require("./books.json");
+
+
+
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+    title: String,
+    author: String,
+    description: String,
+    genre: String,
+    ISBN: Number
+});
+
+
+var Book = mongoose.model('Book', BookSchema);
+
+var book = new Book({title: "Jurassic Park"});
+book.author = "Michael Crichton";
+book.description = "Uhh... Life... Life finds a way.";
+book.genre = 'fiction';
+book.ISBN = 9780345538987;
+//book.save();
+
+
+Book.find({title: "Jurassic Park"}, function(err, found){
+	var foundTitle = found;
+	console.log("Found: \n",foundTitle);
+});
+//TODO: Add three unique books
+
+
+//TODO: Find and display three different books using three different methods
+// For example, find by title, find by author, and find by ISBN.
